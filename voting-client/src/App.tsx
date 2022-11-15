c3af4bff3c13dbb54340e878ae503e95f6885379
Stay <stay@zaroz.cloud>
2022-11-15 23:19:17 +0100

    Added Routing

--- voting-client/src/App.tsx
+++ voting-client/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Layout from './Layout';
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path='create' element={<label>index</label>}/>
+          <Route path=':id'>
+            <Route path='vote' element={<label>vote</label>}/>  
+            <Route path='results' element={<label>results</label>}/>
+            <Route index element={<Navigate replace to="vote"/>}/> {/* REDIRECT TO VOTING PAGE IF NO PAGE WAS SPECIFIED */}
+          </Route>
+          <Route index element={<Navigate replace to="create"/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
