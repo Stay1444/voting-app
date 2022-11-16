3572bc22a90493c4fcfb5510d2453a809efa62a8
Stay <stay@zaroz.cloud>
2022-11-16 20:55:39 +0100

    Added value, onChange & autoFocus props

--- voting-client/src/components/Input.tsx
+++ voting-client/src/components/Input.tsx
@@ -6,13 +6,19 @@ interface InputProps {
     placeholder?: string,
     maxLength?: number,
     className?: string,
-    style?: CSSProperties | undefined
+    style?: CSSProperties | undefined,
+    value?: string,
+    onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined,
+    autoFocus?: boolean
 }
 
 const Input = (props: InputProps) => {
     const themeContext = useContext(ThemeContext)
     return (
         <input
+        autoFocus={props.autoFocus}
+        value={props.value}
+        onChange={props.onChange}
         className={`${style.input} ${style[Themes[themeContext.theme]]} ${props.className}`}
         placeholder={props.placeholder} 
         maxLength={props.maxLength}
