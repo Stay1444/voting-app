30fbffb1edfb9f628c8d2b9aa3aa8afd815b346f
Stay <stay@zaroz.cloud>
2022-11-16 16:44:57 +0100

    Theme switcher

---
+++ voting-client/src/components/ThemeSwitcher.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren, useContext } from "react";
+import { ThemeContext, Themes } from "../ThemeContext";
+
+import style from "./ThemeSwitcher.module.sass"
+
+type ThemeSwitcherProps = PropsWithChildren<unknown>;
+
+import SunIcon from "../img/icons/sun.png"
+import MoonIcon from "../img/icons/moon.png"
+
+const ThemeSwitcher = (props: ThemeSwitcherProps) => {
+    const themeContext = useContext(ThemeContext);
+    return (
+        <>
+            <div 
+                onClick={() => {
+                    themeContext.setTheme(themeContext.theme == Themes.White ?
+                         Themes.Dark : Themes.White);
+                }}
+                
+                className={`${style.container} ${style[Themes[themeContext.theme]]}`}
+                >
+                    <img className={style.image} src={themeContext.theme == Themes.White ? SunIcon : MoonIcon}/>
+            </div>
+            {props.children}
+        </>
+    )
+}
+
+export default ThemeSwitcher;
