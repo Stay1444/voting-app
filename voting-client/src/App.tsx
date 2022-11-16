dc93aad6e45b2e1ac17a96f38a5bf2a8706ce3d5
Stay <stay@zaroz.cloud>
2022-11-16 19:10:49 +0100

    Pages

--- voting-client/src/App.tsx
+++ voting-client/src/App.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './Layout';
+import CreatePage from './pages/CreatePage';
+import ResultsPage from './pages/ResultsPage';
+import VotePage from './pages/VotePage';
 import { ThemeContextProvider } from './ThemeContext';
 
 const App = () => {
@@ -8,10 +11,10 @@ const App = () => {
       <ThemeContextProvider>
         <Routes>
           <Route path='/' element={<Layout/>}>
-            <Route path='create' element={<label>index</label>}/>
+            <Route path='create' element={<CreatePage/>}/>
             <Route path=':id'>
-              <Route path='vote' element={<label>vote</label>}/>  
-              <Route path='results' element={<label>results</label>}/>
+              <Route path='vote' element={<VotePage/>}/>  
+              <Route path='results' element={<ResultsPage/>}/>
               <Route index element={<Navigate replace to="vote"/>}/> {/* REDIRECT TO VOTING PAGE IF NO PAGE WAS SPECIFIED */}
             </Route>
             <Route index element={<Navigate replace to="create"/>}/>
