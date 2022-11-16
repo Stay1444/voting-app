878e1319a96b17f1f59f5352b8fe33f8cd4a566b
Stay <stay@zaroz.cloud>
2022-11-16 20:55:20 +0100

    Added Options, changed some styles

--- voting-client/src/pages/CreatePage.tsx
+++ voting-client/src/pages/CreatePage.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import IconButton from "../components/IconButton";
 import Input from "../components/Input";
 import { ThemeContext, Themes } from "../ThemeContext";
@@ -15,14 +15,51 @@ const TitleStyle = {
     }
 }
 
+interface OptionsProps {
+    value?: string,
+    placeholder?: string,
+    onChange: (value: string) => void,
+    focus?: boolean
+}
+
+const Option = (props: OptionsProps) => {
+
+    return (
+        <Input autoFocus={props.focus} placeholder={props.placeholder} value={props.value} onChange={(e) => {
+            props.onChange(e.target.value);
+        }} maxLength={48}/>
+    )
+}
+
 const CreatePage = () => {
 
     const themeContext = useContext(ThemeContext);
+
+    const [options, setOptions] = useState<string[]>([]);
+    const [focusedOption, setFocusedOption] = useState(-1);
 
     return (
         <>
           <Input className={style.title} placeholder="An Interesting Title..." maxLength={48} style={themeContext.theme == Themes.Dark ? TitleStyle.Dark : TitleStyle.White}/>
         
+            <div className={`${style.options} ${style['options' + Themes[themeContext.theme]]}`}>
+                {options.map((option, index) => (
+                    <Option key={index} value={option} focus={index == focusedOption} onChange={(v) => {
+                        if (v === "") {
+                            options.splice(index, 1);
+                        }else{
+                            options[index] = v;
+                        }
+                        setOptions([...options])
+                    }}/>
+                ))}
+                <Option placeholder="Add Option..." value="" onChange={(v) => {
+                    options.push(v);
+                    setOptions(options);
+                    setFocusedOption(options.length - 1);
+                }}/>
+            </div>
+
             <IconButton icon={faArrowRightToBracket} className={style.create} size='1x'/>
         </>
     )
