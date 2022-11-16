9fe3ccc1884968d5f11b2781976bdc2eefa17559
Stay <stay@zaroz.cloud>
2022-11-16 19:10:28 +0100

    Moved some styles to an object

--- voting-client/src/Layout.tsx
+++ voting-client/src/Layout.tsx
@@ -5,13 +5,42 @@ import { ThemeContext, Themes } from "./ThemeContext";
 import style from "./Layout.module.sass"
 import ThemeSwitcher from "./components/ThemeSwitcher";
 
+import DarkBackground from "./img/backgrounds/Dark.jpg"
+import WhiteBackground from "./img/backgrounds/White.jpg"
+
+const LayoutStyle = {
+    Dark: {
+        backgroundImage: `url(${DarkBackground})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat"
+    },
+
+    White: {
+        backgroundImage: `url(${WhiteBackground})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat"
+    }
+}
+
+const OutletStyle = {
+    Dark: {
+        backgroundColor: "#2c2d3c"
+    },
+
+    White: {
+        backgroundColor: "#ffffff"
+    }
+}
+
 const Layout = () => {
     const themeContext = useContext(ThemeContext);
 
     return (
-        <div className={`${style.main} ${style[Themes[themeContext.theme]]}`}>
+        <div className={`${style.main}`} style={themeContext.theme == Themes.Dark ? LayoutStyle.Dark : LayoutStyle.White}>
             <ThemeSwitcher>
-                <Outlet/>
+                <div className={`${style.outlet}`} style={themeContext.theme == Themes.Dark ? OutletStyle.Dark : OutletStyle.White}>
+                    <Outlet/>
+                </div>
             </ThemeSwitcher>
         </div>
     )
